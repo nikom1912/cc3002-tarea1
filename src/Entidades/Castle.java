6240d0c5fcbbc526ca672bffb52662ea03881d33
Nikom1912 <nikom_1912@hotmail.com>
2018-10-22 01:10:43 -0300

    entidades *

---
+++ src/Entidades/Castle.java
@@ -0,0 +1,8 @@
+package Entidades.Buildings;
+
+import Entidades.Attackable;
+import Entidades.Building;
+
+public class Castle extends Building, implements Attackable {
+
+}
---
+++ src/Entidades/Infantry.java
@@ -0,0 +1,52 @@
+package Entidades.Units;
+
+import Entidades.Attackable;
+import Entidades.Unit;
+
+public class Infantry extends Unit {
+
+    public Infantry(){
+        super(220,70,370);
+
+    }
+    public void attackInfantry(Infantry i){
+        double factor = 1.0;
+        int Damage = (int)(factor* getDMG());
+        i.attacked(Damage);
+    }
+    public void attackArcher(Archer a){
+        double factor = 1.2;
+        int Damage = (int)(factor* getDMG());
+        a.attacked(Damage);
+    }
+    public void attackCavalry(Cavalry c){
+        double factor = 1.2;
+        int Damage = (int)(factor* getDMG());
+        c.attacked(Damage);
+    }
+    public void attackSiege(Siege s){
+        double factor = 1.2;
+        int Damage = (int)(factor* getDMG());
+        s.attacked(Damage);
+    }
+    public void attackVillager(Villager v){
+        double factor = 1.5;
+        int Damage = (int)(factor* getDMG());
+        v.attacked(Damage);
+    }
+    public void attackMonk(Monk m){
+        double factor = 1.0;
+        int Damage = (int)(factor* getDMG());
+        m.attacked(Damage);
+    }
+    public void attackCastle(Castle c){
+        double factor = 0.3;
+        int Damage = (int)(factor* getDMG());
+        c.attacked(Damage);
+    }
+    public void attackBarracks(Barracks b){
+        double factor = 0.7;
+        int Damage = (int)(factor* getDMG());
+        b.attacked(Damage);
+    }
+}
